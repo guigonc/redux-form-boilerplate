b94a76322a7c994eff919e3eb5e63f522e1c49b3
Guilherme Goncalves <ggoncalv@thoughtworks.com>
2017-09-01 10:07:22 -0300

    Minor refactorings

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import SimpleForm from 'app/simpleForm/containers/simpleForm'
 
-const onSubmit = values => alert(JSON.stringify(values))
-
 const App = () => (
-  <SimpleForm onSubmit={onSubmit} />
+  <SimpleForm />
 )
 
 export default App
--- src/app/simpleForm/components/simpleForm.js
+++ src/app/simpleForm/components/simpleForm.js
@@ -3,7 +3,9 @@ import { Field } from 'redux-form'
 import InputText from 'app/simpleForm/components/inputText'
 import validateNotEmpty from 'app/simpleForm/validations/validateNotEmpty'
 
-const SimpleForm = ({ handleSubmit, onSubmit }) => {
+const onSubmit = values => alert(JSON.stringify(values))
+
+const SimpleForm = ({ handleSubmit }) => {
   return (
     <form onSubmit={ handleSubmit(onSubmit) }>
       <Field label="Country" name="country" component={InputText} validate={validateNotEmpty} type="text" />
